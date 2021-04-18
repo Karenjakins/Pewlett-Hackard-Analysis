750d100937c9bbcab31310e774860cef841b4280
karen <79118416+Karenjakins@users.noreply.github.com>
2021-04-18 19:46:22 -0400

    Employees retiring
    Adding Queries

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 -- Creating tables for PH-EmployeesDB
+
 CREATE TABLE departments(
 	dept_no VARCHAR (4) NOT NULL,
 	dept_name VARCHAR (40) NOT NULL,
@@ -14,6 +15,15 @@ CREATE TABLE employees(
 	gender VARCHAR NOT NULL,
 	hire_date DATE NOT NULL, 
 	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp(
+emp_no INT NOT NULL,
+dept_no VARCHAR(6) NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE dept_manager(
@@ -37,20 +47,19 @@ CREATE TABLE salaries(
 
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
-	title INT NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE dept_emp(
-	dept_no VARCHAR NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no)
+	PRIMARY KEY (emp_no, from_date)
 );
 
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+
+DROP TABLE dept_emp CASCADE;
+
+SELECT * FROM titles;
+SELECT * FROM dept_emp;
